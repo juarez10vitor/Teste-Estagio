39b03395380fb653519fae117d1a6cfc7d4c1ad2
juarez10vitor <160255906+juarez10vitor@users.noreply.github.com>
2024-08-30 17:48:18 -0300

    Update Questão4.js

--- Questão4.js
+++ Questão4.js
@@ -1,1 +1,15 @@
+const faturamentoPorEstado = [
+    { estado: 'SP', faturamento: 67836.43 },
+    { estado: 'RJ', faturamento: 36678.66 },
+    { estado: 'MG', faturamento: 29229.88 },
+    { estado: 'ES', faturamento: 27165.48 },
+    { estado: 'Outros', faturamento: 19849.53 }
+];
+
+const totalFaturamento = faturamentoPorEstado.reduce((total, estado) => total + estado.faturamento, 0);
+
+faturamentoPorEstado.forEach(estado => {
+    const percentual = (estado.faturamento / totalFaturamento) * 100;
+    console.log(`Estado: ${estado.estado}, Percentual: ${percentual.toFixed(2)}%`);
+});
 
