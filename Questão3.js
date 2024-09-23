b7fec2d5f7d8254663a806bab553b1f384604b87
Juarez Vitor <160255906+juarezvitor@users.noreply.github.com>
2024-09-23 18:08:33 -0300

    Update Questão3.js

--- Questão3.js
+++ Questão3.js
@@ -1,41 +1,12 @@
-const fs = require('fs');
+//Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA)
 
-fs.readFile('faturamento.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Erro ao ler o arquivo JSON:', err);
-        return;
-    }
+let indice = 12
+let soma = 0
+let k = 1
 
-    const faturamentoDiario = JSON.parse(data);
+while(k < indice){
+    k++
+    soma += K
+}
 
-    let menorFaturamento = Infinity;
-    let maiorFaturamento = -Infinity;
-    let somaFaturamento = 0;
-    let diasComFaturamento = 0;
-
-    for (const item of faturamentoDiario) {
-        const valor = item.faturamento;
-        if (valor != null && valor > 0) {
-            if (valor < menorFaturamento) menorFaturamento = valor;
-            if (valor > maiorFaturamento) maiorFaturamento = valor;
-            somaFaturamento += valor;
-            diasComFaturamento++;
-        }
-    }
-
-    const mediaMensal = somaFaturamento / diasComFaturamento;
-
-    let diasAcimaMedia = 0;
-    for (const item of faturamentoDiario) {
-        const valor = item.faturamento;
-        if (valor != null && valor > mediaMensal) {
-            diasAcimaMedia++;
-        }
-    }
-
-    console.log(`Menor valor de faturamento: ${menorFaturamento}`);
-    console.log(`Maior valor de faturamento: ${maiorFaturamento}`);
-    console.log(`Número de dias com faturamento acima da média mensal: ${diasAcimaMedia}`);
-});
-
-
+console.log(soma) // A soma é: 77
