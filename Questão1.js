667a80bbbb5a4d0329fc8328f8d26074255084b9
Juarez Vitor <160255906+juarezvitor@users.noreply.github.com>
2024-09-23 17:28:06 -0300

    Update Questão1.js

--- Questão1.js
+++ Questão1.js
@@ -1,14 +1,29 @@
-let indice = 13
-let k = 0
-let soma = 0
 
+    function numeroFibonnaci(numero){
+        // Inicializa os primeiros números da sequência de Fibonacci
+        let numA = 0;
+        let numB = 1;
+        //Continua até que numB seja menor que o número fornecido
+        while( numB < numero){
+        //Calcula o próximo número da sequência
+            let numC = numA + numB;
+        // Move os valores para o próximo cálculo
+            numA = numB;
+            numB = numC;
 
+        }
+        // Verifica se o último número calculado é igual ao número fornecido
+        return numB === numero 
 
-while (k < indice){
-    k += 1;
-    soma += k;
-}
-
-console.log(soma)
-
-//Resultado igual a 91
+    }
+    // Pega o número do usuário
+    const numeroUsuario = 7
+    
+    //Chama a função para verificar se o número pertence à sequência
+    if(numeroFibonnaci(numeroUsuario)){
+        //Se pertencer, imprime uma mensagem
+        console.log(`Numero ${numeroUsuario} pertence a sequência Fibonacci`)
+    }else{
+        // Caso contrário, imprima uma mensagem final
+        console.log(`Numero ${numeroUsuario} não pertence a sequência Fibonacci`)
+    }
